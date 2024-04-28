9b67ebe6d880c93522c36f558ee106f00872db68
Everything Sats <everythingsats@gmail.com>
2024-04-28 11:37:14 +0000

    deleted backend

--- Backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://everythingsatoshi1:8hAZWSokaf70Pino@cluster0.4tcwfhu.mongodb.net/food-app').then(()=>console.log('DB connected'))
-}
